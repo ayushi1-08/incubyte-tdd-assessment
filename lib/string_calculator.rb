f48bdfb248659f9b4e182fb5579400fce350dee6
Ayushi Agrawal <ayushiagrawal0501@gmail.com>
2025-04-03 14:34:01 +0530

    Fix: Allow add method to handle new lines correctly as delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,7 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i if numbers.match?(/^\d+$/) # Handles single number / checking
-
-    numbers.split(",").map(&:to_i).sum # Handles two numbers
+   
+    numbers.split(/[\n,]/).map(&:to_i).sum
   end
 end
